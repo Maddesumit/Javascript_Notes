d6b88f3cbadb1796491d1e89ff53d732b69bb17d
Maddesumit <maddesumit@gmail.com>
2023-10-04 21:30:43 +0530

    notes

--- Javascript_Basics/03.datatypeConversion.js
+++ Javascript_Basics/03.datatypeConversion.js
@@ -37,6 +37,10 @@ console.log(boolean3)
 "Sumit"=>String to Boolean=> true
 ""=>Empty String to boolean ==> false
 1=> true
-0=>false
+0=>false*/
 
-*/
+//Number to String
+let someNumber=33
+let string1=String(someNumber)
+console.log(string1)
+console.log(typeof string1) //"33"
